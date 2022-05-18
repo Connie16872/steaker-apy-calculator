f19332991821220f7c781442f80bbc7234c376b8
Connie16872 <connie16872@gmail.com>
2022-05-18 15:51:37 +0800

    Refine script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function cal() {
-  var amount = document.getElementById("p1").value;
-  var days = document.getElementById("p2").value;
-  var apy = document.getElementById("p3").value;
-  var result = (amount * Math.pow( (1+(apy/100)) , (days/365) ) );
-  var showAns = document.getElementById("answer").value;
-  document.getElementById("answer").innerHTML = ("期滿後可以獲得 " + result.toFixed(6) + " (本金加利息)");
-  }
+const getCalculatedApy = () => {
+  const amount = document.getElementById('amount').value
+  const days = document.getElementById('days').value
+  const apy = document.getElementById('apy').value
+  return amount * Math.pow(1 + apy / 100, days / 365)
+}
+
+const renderAnswer = (apy) => {
+  const answerDom = document.getElementById('answer')
+  answerDom.innerHTML = '期滿後可以獲得 ' + apy.toFixed(6) + ' (本金加利息)'
+}
+
+const runApyApp = () => {
+  const calculatedApy = getCalculatedApy()
+  renderAnswer(calculatedApy)
+}
